d9c6406ed39817ee309dfe06bd6897445f286fd1
moat <temoat@gmail.com>
2012-05-10 18:47:19 -0700

    changed 'recv' to send a typing event. added some debugging stuff

--- main.py
+++ main.py
@@ -21,16 +21,19 @@ while True:
     log("Stranger: " + ev)
     while True:
       try:
+        om.send_typing_event()
         resp = cb.Ask(ev)
         resp = re.sub("cleverbot", "Tim the Enchanter", resp, re.IGNORECASE)
         break
       except cleverbot.ServerFullError:
+        print "DEBUG: cleverbot.ServerFullError"
         continue
     log("Cleverbot: " + resp)
     if om.connected and len(resp) > 0:
         om.send_msg(resp)
 
   om.connect("message-received", recv)
+  om.connect("debug", debug)
 
   om.start(threaded = False)
   log("*****Conversation End*****")
